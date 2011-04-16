0fd9d0ae136c64dcd6656d1a5ce9880ed1c85b11
Johannes Goller <j.goller@digital-science.com>
2011-04-17 00:04:52 +0900

    Correcting import statements and completing basic implementation.

--- src/feed_update.py
+++ src/feed_update.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-import feedparser
+from sqlalchemy.types import Integer,String
+from sqlalchemy import Column
+from sqlalchemy.orm import sessionmaker
+# import feedparser
 
 Base = declarative_base()
 class FeedSource(Base):
@@ -9,7 +12,17 @@ class FeedSource(Base):
     id   = Column(Integer,primary_key=True)
     name = Column(String)
     url  = Column(String)
+    
+    def __init__(self,name,url):
+        self.name = name
+        self.url  = url
+    
+    def __repr__(self):
+        return u'FeedSource("%s","%s")' % (self.name,self.url)
 
 
 def main():
     engine = create_engine('sqlite:///test',echo=True)
+    session = sessionmaker(bind=engine)
+    session.add(FeedSource('Asahi 政治','http://rss.asahi.com/f/asahi_politics'))
+    session.commit()
