e97aaf4610a5f362b85d4647b37dc9d8e3107626
Johannes Goller <jogojapan@gmail.com>
2011-04-21 09:40:59 +0900

    Module for simple HTML language/encoding analysis.

---
+++ src/langid/html.py
@@ -0,0 +1,41 @@
+'''
+Created on 2011/04/21
+
+@author: gollerjo
+'''
+
+from HTMLParser import HTMLParser
+# from HTMLParser import HTMLParseError
+import re
+
+class HtmlLangid(HTMLParser):
+    '''
+    Analyzes the meta data of an HTML document to extract
+    information about its language and character encoding.
+    '''
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self._encoding_pattern = re.compile('charset\s*=\s*([^\s\x22\x27]+)')
+        self.reset()
+
+    def reset(self):
+        HTMLParser.reset(self)
+        self._result_language = ''
+        self._result_encoding = ''
+
+    def handle_starttag(self,tag,attrs):
+        if tag == 'meta':
+            http_equiv = ''
+            content    = ''
+            for (key,val) in attrs:
+                if key == 'http-equiv':
+                    http_equiv = val
+                elif key == 'content':
+                    content = val
+            if http_equiv == 'content-language':
+                self._result_language = content
+            elif http_equiv == 'content-type':
+                m = self._encoding_pattern.search(content)
+                if m:
+                    self._result_encoding = m.group(1)
