8373450f35cd86af0c58cce28364da184c27c625
Johannes Goller <jogojapan@gmail.com>
2011-06-13 00:08:16 +0900

    Bettern naming.

--- src/qitemlist.py
+++ src/qitemlist.py
@@ -6,8 +6,7 @@ Created on 2011/04/17
 @author: gollerjo
 '''
 
-from coffer import Coffer
-from PyQt4 import QtGui,QtCore
+from PyQt4 import QtGui
 
 class QItemList(QtGui.QDockWidget):
     def __init__(self,parent,coffer,feed_source):
@@ -19,10 +18,11 @@ class QItemList(QtGui.QDockWidget):
             table_data.append(item)
         item_table = QtGui.QTableWidget(len(table_data),2,self)
         row = 0
-        for (feed_id,entry_id,entry) in table_data:
+        for (_feed_id,_entry_id,entry) in table_data:
             item_table.setItem(row,0,QtGui.QTableWidgetItem(entry.title))
-            item_table.setItem(row,1,QtGui.QTableWidgetItem(feed_id))
+            item_table.setItem(row,1,QtGui.QTableWidgetItem(entry.link))
             row += 1
         item_table.resizeColumnsToContents()
+        item_table.resizeRowsToContents()
         self.setWidget(item_table)
         self.item_table = item_table
